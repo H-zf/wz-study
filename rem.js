e5b09e16bbeac888cc639d7e91c9b5e5806cbe92
H-zf <cxy_hezhongfan@163.com>
2022-06-27 21:12:11 +0800

    Update rem.js

--- rem.js
+++ rem.js
@@ -20,3 +20,27 @@ let unitBodyFontsize
   doc.addEventListener('DOMContentLoaded', recalc, false)
   recalc()
 })(document, window)
+
+;(function(doc, win) {
+  const documentEle = doc.documentElement
+  const bodys = document.getElementsByTagName('body')[0]
+  const resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
+  const _scale = isIOS() ? 1 : win.getComputedStyle(documentEle)['font-size'].replace(/px/gi, '') / 50
+  const recalc = function() {
+    const clientWidth = documentEle.clientWidth
+    if (!clientWidth) return
+    if (clientWidth <= 1080) {
+      unitBodyFontsize = 100 * (clientWidth / 750)
+      documentEle.style.fontSize = unitBodyFontsize / _scale + 'px'
+    } else {
+      unitBodyFontsize = 100 * (1080 / 750)
+      documentEle.style.fontSize = unitBodyFontsize / _scale + 'px'
+    }
+    bodys.style.visibility = 'visible'
+    store.commit('SET_RemUnitSize', unitBodyFontsize / _scale)
+  }
+  if (!doc.addEventListener) return
+  win.addEventListener(resizeEvt, recalc, false)
+  doc.addEventListener('DOMContentLoaded', recalc, false)
+  recalc()
+})(document, window)
